73f64374bded25bfc1c3d6a0f737b2710b4c86f5
Alexboni97 <alexboni@ucm.es>
2025-10-26 14:45:46 +0100

    init booksResultListAdapter

---
+++ Practica 2/GoogleBooksClient/app/src/main/java/es/ucm/fdi/pad/googlebooksclient/modelo/BooksResultListAdapter.java
@@ -0,0 +1,25 @@
+package es.ucm.fdi.pad.googlebooksclient.modelo;
+
+import android.view.ViewGroup;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class BooksResultListAdapter extends RecyclerView.Adapter<BooksResultListAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public BooksResultListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BooksResultListAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
