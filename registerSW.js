784654879408fc5862ef157a38af6988ff5e3ce8
BONI <100862113+alex-boni@users.noreply.github.com>
2025-12-12 23:23:13 +0100

    Update registerSW.js

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/PAD-2025-26-G02-Practicas/sw.js', { scope: '/' })})}
